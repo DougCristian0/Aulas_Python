0a69d5b5ef220c69d88b19aa6d6d84714934f5f2
DougCristian0 <165964701+DougCristian0@users.noreply.github.com>
2025-10-11 09:56:29 -0300

    Upload exemplos lista
    Upload de arquivo com exemplos de lista feitos na aula dia 11/10

---
+++ Python_Basico/Lista.py
@@ -0,0 +1,43 @@
+lista = ["Local", 1232, True]
+
+#           0         1         2
+nomes = ["Fabio", "Danilo", "Valter", "Joao"]
+numeros = [231, 1242, 4324, 321]
+
+nomes.remove("Valter")
+print(nomes)
+
+# for e, nome in enumerate(nomes):
+#     print(e, nomes)
+# print(enumerate(nomes))
+
+
+# nomes.sort()
+# nomes.reverse()
+# print(nomes)
+
+# numeros.sort()
+# print(numeros)
+
+
+# if "Taciano" in nomes:
+#     nomes[nomes.index("Taciano")] = "Pedro"
+#     print(nomes)
+# else:
+#     print("Não tem Taciano")
+
+
+#nomes.insert(1, "Danilo")
+# cont = 0
+# for nome in nomes:
+#     if nome == "Taciano":
+#         nomes[cont] = "Pedro"
+#     cont+=1
+
+
+#nomes.append("Marcos")
+#nomes.append("Maria")
+
+
+#if nome in nomes:
+#    print(nomes.count("Douglas"))
