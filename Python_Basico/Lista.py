b5bbc15a7f5f113db3c14ff89b31206951b69736
DougCristian0 <165964701+DougCristian0@users.noreply.github.com>
2025-10-11 09:57:47 -0300

    Upload de arquivo
    Upload do arquivo corrigido com exemplos da aula 11/10

--- Python_Basico/Lista.py
+++ Python_Basico/Lista.py
@@ -4,40 +4,42 @@ lista = ["Local", 1232, True]
 nomes = ["Fabio", "Danilo", "Valter", "Joao"]
 numeros = [231, 1242, 4324, 321]
 
-nomes.remove("Valter")
-print(nomes)
+def remove():
+    nomes.remove("Valter")
+    print(nomes)
 
-# for e, nome in enumerate(nomes):
-#     print(e, nomes)
-# print(enumerate(nomes))
+def Enumerate():
+    for e, nome in enumerate(nomes):
+        print(e, nomes)
+    print(enumerate(nomes))
 
+def ordena():
+    nomes.sort()
+    nomes.reverse()
+    print(nomes)
 
-# nomes.sort()
-# nomes.reverse()
-# print(nomes)
+    numeros.sort()
+    print(numeros)
 
-# numeros.sort()
-# print(numeros)
+def trocaNome():
+    if "Taciano" in nomes:
+        nomes[nomes.index("Taciano")] = "Pedro"
+        print(nomes)
+    else:
+        print("Não tem Taciano")
 
+def insere():
+    nomes.insert(1, "Danilo")
+    cont = 0
+    for nome in nomes:
+        if nome == "Taciano":
+            nomes[cont] = "Pedro"
+        cont+=1
 
-# if "Taciano" in nomes:
-#     nomes[nomes.index("Taciano")] = "Pedro"
-#     print(nomes)
-# else:
-#     print("Não tem Taciano")
+def insereNoFinal():
+    nomes.append("Marcos")
+    nomes.append("Maria")
 
-
-#nomes.insert(1, "Danilo")
-# cont = 0
-# for nome in nomes:
-#     if nome == "Taciano":
-#         nomes[cont] = "Pedro"
-#     cont+=1
-
-
-#nomes.append("Marcos")
-#nomes.append("Maria")
-
-
-#if nome in nomes:
-#    print(nomes.count("Douglas"))
+def contaQuantosNaLista():
+    if "Douglas" in nomes:
+        print(nomes.count("Douglas"))
